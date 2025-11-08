f1c27ce8de384c1097a4f220b30b3d43a0082a9b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2025-11-08 01:06:55 -0300

    criação do projeto java "gestao_empresarial"

---
+++ backend/gestao-empresarial/src/test/java/com/sistema/gestao_empresarial/GestaoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.gestao_empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
